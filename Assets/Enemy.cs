0c6033d6fe7b2283a5efc47d4b69dafef11ae7bc
chips <chips@metrica.se>
2019-10-07 20:22:01 +0200

    x_x

---
+++ Assets/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    CubePhysics _cubePhysics;
+
+    void Start()
+    {
+        _cubePhysics = gameObject.GetComponent<CubePhysics>();
+    }
+
+    void Update()
+    {
+        var force = new Vector3(1.0f, 0.0f, 0.0f);
+        if (force.sqrMagnitude > 0.0f)
+        {
+            _cubePhysics.InputForce = force * 10.0f;
+        }
+    }
+}
